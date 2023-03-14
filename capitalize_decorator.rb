dd48a62598538b0fce8daaa28aae56c2f12c411e
Allassane <dickoallassane1997@gmail.com>
2023-03-14 16:37:50 +0100

    Update capitalize_decorator to capitalize only the first letter

--- capitalize_decorator.rb
+++ capitalize_decorator.rb
@@ -1,6 +1,6 @@
 require './correct_nameable'
 class CapitalizeDecorator < CorrectNameable
   def correct_name
-    @person.correct_name.upcase
+    @person.correct_name[0].upcase + @person.correct_name[1..@person.correct_name.length]
   end
 end
