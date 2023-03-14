b8707b6af6271d54140cc7c103547417c9ed3700
Allassane <dickoallassane1997@gmail.com>
2023-03-14 18:11:18 +0100

    Update trimmer decorator

--- trimmer_decorator.rb
+++ trimmer_decorator.rb
@@ -1,7 +1,6 @@
 require './correct_nameable'
 class TrimmerDecorator < CorrectNameable
   def correct_name
-    name = @person.correct_name.strip
-    name[0..9]
+    @person.correct_name.length > 10 ? @person.correct_name[0..9] : @person.correct_name
   end
 end
