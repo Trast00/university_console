a83fac576d43b424b438644512ef0f6416529943
Mukama Steven <stevenmukama1@gmail.com>
2023-03-22 16:55:53 +0200

    add test for classroom and rentals

---
+++ spec/classroom_spec.rb
@@ -0,0 +1,22 @@
+require_relative './spec_helper'
+
+describe Classroom do
+  context "test classroom specific methods" do
+    before(:all) do
+      @classroom = Classroom.new("A4")
+      @student = Student.new(17, "Rose")
+    end
+
+    it "test classroom instance" do
+      expect(@classroom).to be_instance_of Classroom
+    end
+
+    it "test add student to classroom" do
+      @classroom.add_student(@student)
+
+      expect(@classroom.list_student.include?(@student)).to be true
+
+    end
+
+  end
+end
