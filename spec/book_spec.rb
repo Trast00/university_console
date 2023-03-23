0b2efab825eb55b788dfa05a4b73711047e909f2
Mukama Steven <stevenmukama1@gmail.com>
2023-03-23 12:27:34 +0200

    Add more tests for book_spec.rb file

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -13,3 +13,33 @@ describe Person do
     end
   end
 end
+
+describe '#add_rental' do
+  let(:person) { Person.new('John') }
+  let(:rental) { 'rental' }
+
+  it 'adds rental to list_rental' do
+    person.add_rental(rental)
+    expect(person.list_rental).to eq([rental])
+  end
+
+  it 'does not add rental to list_rental if it already exists' do
+    person.list_rental << rental
+    person.add_rental(rental)
+    expect(person.list_rental).to eq([rental])
+  end
+end
+
+describe '#to_json' do
+  let(:book) { Book.new('The Great Gatsby', 'F.Scott Fitzgerald') }
+
+  it 'returns a hash with title and author keys' do
+    expect(book.to_json).to include(:title, :author)
+  end
+
+  it 'returns a hash with correct title and author values' do
+    expect(book.to_json[:title]).to eq('The Great Gatsby')
+
+    expect(book.to_json[:author]).to eq('F.Scott Fitzgerald')
+  end
+end
